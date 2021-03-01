8776f78535a5361389db2d985e662a46d18d5bad
Gayath1 <chandulagayan@gmail.com>
2021-03-01 11:23:26 +0530

    ADD Session preview

--- src/main/java/com/mycompany/itpm_timetable_management/SessionRooms.java
+++ src/main/java/com/mycompany/itpm_timetable_management/SessionRooms.java
@@ -5,6 +5,9 @@
  */
 package com.mycompany.itpm_timetable_management;
 
+import static com.mycompany.itpm_timetable_management.EditRooms.DB_URL;
+import static com.mycompany.itpm_timetable_management.EditRooms.password;
+import static com.mycompany.itpm_timetable_management.EditRooms.username;
 import static com.mycompany.itpm_timetable_management.LecturerTimeTable.DB_URL;
 import static com.mycompany.itpm_timetable_management.LecturerTimeTable.password;
 import static com.mycompany.itpm_timetable_management.LecturerTimeTable.username;
@@ -13,6 +16,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -31,6 +35,8 @@ public class SessionRooms extends javax.swing.JFrame {
     public SessionRooms() {
         initComponents();
         currentSession();
+        currentRoom();
+        
     }
 
     Connection conn;
@@ -41,10 +47,49 @@ public class SessionRooms extends javax.swing.JFrame {
         try {    
             Class.forName("com.mysql.jdbc.Driver");
             conn = DriverManager.getConnection(DB_URL, username, password);
-            ResultSet rs = conn.createStatement().executeQuery("SELECT * FROM session");              
+            String grp = (String)jComboBox3.getSelectedItem();
+            if("consecutivesession".equals(grp)){
+                
+                jLabel2.setText("Select consecutive Session");
+                Statement statement = conn.createStatement();
+                String query = "SELECT * FROM consecutive_session ";
+                ResultSet rs = statement.executeQuery(query);
+
+                while (rs.next())
+                {                             
+                   String name = rs.getString("id");
+                   jComboBox1.addItem(name);
+                }//end while
+                
+             }else{
+                jLabel2.setText("Select Session");
+                Statement statement = conn.createStatement();
+                String query = "SELECT * FROM session ";
+                ResultSet rs = statement.executeQuery(query);
+
+                while (rs.next())
+                {                             
+                   String name = rs.getString("id");
+                   jComboBox1.addItem(name);
+                }//end while
+             }
+            
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(SessionRooms.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(SessionRooms.class.getName()).log(Level.SEVERE, null, ex);
+        }        
+    }
+    
+    private void currentRoom(){
+           
+        try {    
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(DB_URL, username, password);
+            ResultSet rs = conn.createStatement().executeQuery("SELECT * FROM rooms");              
             while(rs.next()){     
-                String name = rs.getString("id");
-                jComboBox1.addItem(name);;
+                String name = rs.getString("room_name");
+                jComboBox2.addItem(name);
             }
             
         } catch (ClassNotFoundException ex) {
@@ -53,6 +98,59 @@ public class SessionRooms extends javax.swing.JFrame {
             Logger.getLogger(SessionRooms.class.getName()).log(Level.SEVERE, null, ex);
         }        
     }
+    
+        private void currentpreview(){
+           
+        try {    
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(DB_URL, username, password);
+            String grp = (String)jComboBox3.getSelectedItem();
+            String id = (String)jComboBox1.getSelectedItem();
+            if("consecutivesession".equals(grp)){
+                
+                jLabel2.setText("Select consecutive Session");
+                Statement statement = conn.createStatement();
+                String query = "SELECT * FROM consecutive_session WHERE id = '"+id+"' ";
+                ResultSet rs = statement.executeQuery(query);
+
+                while (rs.next())
+                {                             
+                   String subject = rs.getString("subject");
+                   jLabel4.setText(subject);
+                   String lecturers = rs.getString("lecturers");
+                   jLabel5.setText(lecturers);
+                   String tags = rs.getString("tags");
+                   jLabel6.setText(tags);
+                   String groups = rs.getString("groups");
+                   jLabel7.setText(groups);
+                }//end while
+                
+             }else{
+                jLabel2.setText("Select Session");
+                Statement statement = conn.createStatement();
+                String query = "SELECT * FROM session WHERE id = '"+id+"' ";
+                ResultSet rs = statement.executeQuery(query);
+
+                while (rs.next())
+                {                             
+                   String subject = rs.getString("subject");
+                   jLabel4.setText(subject);
+                   String lecturers = rs.getString("lecturers");
+                   jLabel5.setText(lecturers);
+                   String tags = rs.getString("tags");
+                   jLabel6.setText(tags);
+                   String groups = rs.getString("groups");
+                   jLabel7.setText(groups);
+                }//end while
+             }
+            
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(SessionRooms.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(SessionRooms.class.getName()).log(Level.SEVERE, null, ex);
+        }        
+    }
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -76,6 +174,8 @@ public class SessionRooms extends javax.swing.JFrame {
         jLabel8 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
+        jLabel9 = new javax.swing.JLabel();
+        jComboBox3 = new javax.swing.JComboBox<>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -84,6 +184,12 @@ public class SessionRooms extends javax.swing.JFrame {
 
         jLabel2.setFont(new java.awt.Font("Nexa Bold", 0, 24)); // NOI18N
         jLabel2.setText("Select Session");
+
+        jComboBox1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jComboBox1ActionPerformed(evt);
+            }
+        });
 
         jLabel3.setFont(new java.awt.Font("Nexa Bold", 0, 24)); // NOI18N
         jLabel3.setText("Select Room");
@@ -146,33 +252,54 @@ public class SessionRooms extends javax.swing.JFrame {
         jButton2.setBorderPainted(false);
         jButton2.setContentAreaFilled(false);
         jButton2.setFocusPainted(false);
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        jLabel9.setFont(new java.awt.Font("Nexa Bold", 0, 24)); // NOI18N
+        jLabel9.setText("Session Type");
+
+        jComboBox3.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jComboBox3.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Session", "consecutivesession" }));
+        jComboBox3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jComboBox3ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(28, 28, 28)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jLabel2)
-                        .addGap(18, 18, 18)
-                        .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 213, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(159, 159, 159)
-                        .addComponent(jLabel3))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jButton2)))
-                .addGap(18, 18, 18)
-                .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, 213, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(220, Short.MAX_VALUE))
-            .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(407, 407, 407)
                 .addComponent(jLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jButton1)
                 .addGap(39, 39, 39))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(28, 28, 28)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(100, 100, 100)
+                        .addComponent(jButton2))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel9))
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jComboBox3, javax.swing.GroupLayout.PREFERRED_SIZE, 213, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 213, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(158, 158, 158)
+                                .addComponent(jLabel3)
+                                .addGap(18, 18, 18)
+                                .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, 213, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                .addContainerGap(221, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -184,20 +311,24 @@ public class SessionRooms extends javax.swing.JFrame {
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(21, 21, 21)
                         .addComponent(jButton1)))
-                .addGap(85, 85, 85)
+                .addGap(55, 55, 55)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel9)
+                    .addComponent(jComboBox3, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(51, 51, 51)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
                     .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel3)
                     .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 22, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 25, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(33, 33, 33))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                         .addComponent(jButton2)
-                        .addGap(123, 123, 123))))
+                        .addGap(123, 123, 123))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap())))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -218,6 +349,26 @@ public class SessionRooms extends javax.swing.JFrame {
         // TODO add your handling code here:
         setVisible(false);
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jComboBox3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox3ActionPerformed
+        // TODO add your handling code here:
+        jComboBox1.removeAllItems();
+        currentSession();
+    }//GEN-LAST:event_jComboBox3ActionPerformed
+
+    private void jComboBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox1ActionPerformed
+        // TODO add your handling code here:
+        jLabel4.setText("");
+        jLabel5.setText(""); 
+        jLabel6.setText(""); 
+        jLabel7.setText(""); 
+        jLabel8.setText(""); 
+        currentpreview();
+    }//GEN-LAST:event_jComboBox1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -259,6 +410,7 @@ public class SessionRooms extends javax.swing.JFrame {
     private javax.swing.JButton jButton2;
     private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JComboBox<String> jComboBox2;
+    private javax.swing.JComboBox<String> jComboBox3;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -267,6 +419,7 @@ public class SessionRooms extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     // End of variables declaration//GEN-END:variables
