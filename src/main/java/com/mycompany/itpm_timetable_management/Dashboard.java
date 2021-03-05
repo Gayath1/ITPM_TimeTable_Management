65a03310588f4f368dac12f6c1f1dbf47ef8f1b0
Gayath1 <chandulagayan@gmail.com>
2021-03-05 10:51:45 +0530

    dashboard changes

--- src/main/java/com/mycompany/itpm_timetable_management/Dashboard.java
+++ src/main/java/com/mycompany/itpm_timetable_management/Dashboard.java
@@ -41,6 +41,8 @@ public class Dashboard extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+
         jLabel1.setFont(new java.awt.Font("Nexa Bold", 0, 48)); // NOI18N
         jLabel1.setText("Time Table Management System");
 
