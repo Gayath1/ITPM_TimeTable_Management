943f3ede722abe3e30c3f7b53214908f8221d024
Gayath1 <chandulagayan@gmail.com>
2021-03-08 10:53:45 +0530

    changes

--- src/main/java/com/mycompany/itpm_timetable_management/AddSession.java
+++ src/main/java/com/mycompany/itpm_timetable_management/AddSession.java
@@ -170,6 +170,7 @@ public class AddSession extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
 
