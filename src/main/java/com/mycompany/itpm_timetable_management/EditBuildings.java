35592c16ebf5b57440dab2237b3967c8eb9494ec
Gayath1 <chandulagayan@gmail.com>
2021-03-10 10:13:40 +0530

    changes

--- src/main/java/com/mycompany/itpm_timetable_management/EditBuildings.java
+++ src/main/java/com/mycompany/itpm_timetable_management/EditBuildings.java
@@ -56,6 +56,7 @@ public class EditBuildings extends javax.swing.JFrame {
         jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
 
