a0baeac2562666f1216c13afad2fff71f68ce738
Gayath1 <chandulagayan@gmail.com>
2021-03-10 10:13:17 +0530

    changes

--- src/main/java/com/mycompany/itpm_timetable_management/EditBuildings.java
+++ src/main/java/com/mycompany/itpm_timetable_management/EditBuildings.java
@@ -57,6 +57,8 @@ public class EditBuildings extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+
         jLabel1.setFont(new java.awt.Font("Nexa Bold", 0, 48)); // NOI18N
         jLabel1.setText("Edit Buildings");
 
