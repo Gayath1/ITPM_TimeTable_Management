08663c21ec7aeae4ec831379845044b08b368387
Gayath1 <chandulagayan@gmail.com>
2021-03-11 12:13:37 +0530

    changes

--- src/main/java/com/mycompany/itpm_timetable_management/StudentTimeTable.java
+++ src/main/java/com/mycompany/itpm_timetable_management/StudentTimeTable.java
@@ -74,6 +74,7 @@ public class StudentTimeTable extends javax.swing.JFrame {
         jTable1 = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
 
         jLabel1.setFont(new java.awt.Font("Nexa Bold", 0, 24)); // NOI18N
         jLabel1.setText("Select Group");
@@ -174,6 +175,7 @@ public class StudentTimeTable extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
