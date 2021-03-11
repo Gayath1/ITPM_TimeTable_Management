690670293d111b2928c28c92aaf9d79aab6d2a15
Gayath1 <chandulagayan@gmail.com>
2021-03-11 12:15:52 +0530

    changes

--- src/main/java/com/mycompany/itpm_timetable_management/SystemStatistics.java
+++ src/main/java/com/mycompany/itpm_timetable_management/SystemStatistics.java
@@ -35,6 +35,9 @@ public class SystemStatistics extends javax.swing.JFrame {
         jButton4 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
 
         jLabel1.setFont(new java.awt.Font("Nexa Bold", 0, 48)); // NOI18N
         jLabel1.setText("Statistics Dashboard");
@@ -131,6 +134,7 @@ public class SystemStatistics extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
