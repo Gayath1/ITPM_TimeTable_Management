d7714b364304a31567c5258c49b9048948b24332
Gayath1 <chandulagayan@gmail.com>
2021-03-11 12:08:38 +0530

    changes

--- src/main/java/com/mycompany/itpm_timetable_management/LocationTimeTable.java
+++ src/main/java/com/mycompany/itpm_timetable_management/LocationTimeTable.java
@@ -75,6 +75,8 @@ public class LocationTimeTable extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+
         jLabel1.setFont(new java.awt.Font("Nexa Bold", 0, 24)); // NOI18N
         jLabel1.setText("Select Room");
         jLabel1.setToolTipText("");
