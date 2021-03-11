508dcbe7824f5674b9f1406eac472618ff780f99
Gayath1 <chandulagayan@gmail.com>
2021-03-11 12:16:11 +0530

    changes

--- src/main/java/com/mycompany/itpm_timetable_management/TimeTableDashboard.java
+++ src/main/java/com/mycompany/itpm_timetable_management/TimeTableDashboard.java
@@ -34,6 +34,7 @@ public class TimeTableDashboard extends javax.swing.JFrame {
         jButton4 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
 
         jLabel1.setFont(new java.awt.Font("Nexa Bold", 0, 48)); // NOI18N
         jLabel1.setText("TimeTable Dashboard");
@@ -124,6 +125,7 @@ public class TimeTableDashboard extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
