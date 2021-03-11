901e9fa9cccef303f422ad0539c08912397d9eb4
Gayath1 <chandulagayan@gmail.com>
2021-03-11 12:11:55 +0530

    changes

--- src/main/java/com/mycompany/itpm_timetable_management/SearchSession.java
+++ src/main/java/com/mycompany/itpm_timetable_management/SearchSession.java
@@ -81,6 +81,8 @@ public class SearchSession extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+
         jLabel1.setFont(new java.awt.Font("Nexa Bold", 0, 48)); // NOI18N
         jLabel1.setText("Search Session");
 
